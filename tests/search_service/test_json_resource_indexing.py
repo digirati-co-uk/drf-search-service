f17fe59830b570f8ae88a40a0af2cc9f23d3e977
Finlay McCourt <finlaymccourt@gmail.com>
2022-02-09 11:22:09 +0000

    minor test additions

--- tests/search_service/test_json_resource_indexing.py
+++ tests/search_service/test_json_resource_indexing.py
@@ -25,4 +25,35 @@ def test_json_resource_create(http_service):
     assert response.status_code == status
     assert response_json.get("label") == post_json.get("label")
     assert response_json.get("data") == post_json.get("data")
+    assert response_json.get("created") != None
+    assert response_json.get("modified") != None
+    assert response_json.get("id") != None
+    test_data_store["json_resource_id"] = response_json.get("id")
+
+
+def test_json_resource_get(http_service):
+    """ """
+    test_endpoint = "json_resource"
+    status = 200
+    resource_id = test_data_store.get('json_resource_id')
+    response = requests.get(
+        f"{http_service}/{app_endpoint}/{test_endpoint}/{resource_id}",
+        headers=test_headers
+    )
+    response_json = response.json()
+    assert response.status_code == status
+    assert response_json.get("id") == resource_id
+
+def test_json_resource_indexables_creation(http_service):
+    """ """
+    test_endpoint = "indexables"
+    status = 200
+    resource_id = test_data_store.get('json_resource_id')
+    response = requests.get(
+        f"{http_service}/{app_endpoint}/{test_endpoint}/{resource_id}",
+        headers=test_headers
+    )
+    response_json = response.json()
+    assert response.status_code == status
+    assert response_json.get("id") == resource_id
 
